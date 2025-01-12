36fbf1310ef40fe17fab74934014b1b01f7d427e
gravetogeek <edusrm11@gmail.com>
2025-01-11 23:58:02 -0300

    :recycle: refactor: Refatoração do módulo app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
+import {ApolloDriver,ApolloDriverConfig} from '@nestjs/apollo'
 import {Module} from '@nestjs/common'
+import {ConfigModule} from '@nestjs/config'
+import {GraphQLModule} from '@nestjs/graphql'
+import * as path from 'path'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
+import {DatabaseModule} from './database/database.module'
+import {AppointmentsResolver} from './resolvers/appointments-resolvers'
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot(),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            autoSchemaFile: path.resolve(process.cwd(),'prisma/schema.gql'),
+            driver: ApolloDriver,
+            sortSchema: true,
+            debug: true,
+            playground: true,
+            introspection: true,
+            installSubscriptionHandlers: true,
+        }),
+        DatabaseModule,
+    ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService,AppointmentsResolver],
 })
 export class AppModule {}
