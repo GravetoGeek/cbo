3e79b8a2cd2abd74f8176c1d2d269860e963c0aa
gravetogeek <edusrm11@gmail.com>
2025-01-15 21:52:54 -0300

    :construction: Implementando Arquitetura Hexagonal, incluindo familia.entity.ts

---
+++ src/cbo/domain/model/entities/familia.entity.ts
@@ -0,0 +1,11 @@
+export class Familia {
+    id: string
+    codigo: string
+    titulo: string
+    constructor(id: string, codigo: string, titulo: string) {
+        this.id = id
+        this.codigo = codigo
+        this.titulo = titulo
+    }
+}
+
