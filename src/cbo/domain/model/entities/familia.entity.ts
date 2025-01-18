ad93ee7d77530728b248ecb933ca2fcbd77d99bb
gravetogeek <edusrm11@gmail.com>
2025-01-17 23:50:31 -0300

    :recycle: refactor: Aprimorando a classe familia

--- src/cbo/domain/model/entities/familia.entity.ts
+++ src/cbo/domain/model/entities/familia.entity.ts
@@ -1,11 +1,21 @@
+import {Ocupacao} from './ocupacao.entity'
+import {PerfilOcupacional} from './perfil-ocupacional.entity'
+import {SubGrupo} from './sub-grupo.entity'
+
 export class Familia {
-    id: string
+    id?: string
     codigo: string
     titulo: string
-    constructor(id: string, codigo: string, titulo: string) {
-        this.id = id
+    subGrupoId?: string
+    subGrupo?: SubGrupo
+    ocupacoes?: Ocupacao[] // Opcional, pois pode não haver ocupações associadas inicialmente
+    perfilOcupacional?: PerfilOcupacional[] // Opcional
+    constructor(codigo: string, titulo: string) {
         this.codigo = codigo
         this.titulo = titulo
     }
-}
 
+    public toString(): string {
+        return `ID: ${this.id}, Família: ${this.titulo}, Código: ${this.codigo}`
+    }
+}
