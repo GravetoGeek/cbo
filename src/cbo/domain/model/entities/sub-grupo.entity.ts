6c7e44a0928b99773d203a7edd3ce097edec3248
gravetogeek <edusrm11@gmail.com>
2025-01-17 23:51:36 -0300

    :recycle: refactor: Aprimorando a classe  subgrupo

--- src/cbo/domain/model/entities/sub-grupo.entity.ts
+++ src/cbo/domain/model/entities/sub-grupo.entity.ts
@@ -1,9 +1,16 @@
+import {Familia} from './familia.entity'
+import {PerfilOcupacional} from './perfil-ocupacional.entity'
+import {SubGrupoPrincipal} from './sub-grupo-principal.entity'
+
 export class SubGrupo {
-    id: string
+    id?: string
     codigo: string
     titulo: string
-    constructor(id: string, codigo: string, titulo: string) {
-        this.id = id
+    subGrupoPrincipalId?: string
+    subGrupoPrincipal?: SubGrupoPrincipal
+    familias?: Familia[] // Opcional
+    perfilOcupacional?: PerfilOcupacional[] // Opcional
+    constructor( codigo: string, titulo: string) {
         this.codigo = codigo
         this.titulo = titulo
     }
