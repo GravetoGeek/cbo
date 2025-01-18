5635a1ca9bd4c674d9a06481c84edfba57ed7aa7
gravetogeek <edusrm11@gmail.com>
2025-01-17 23:51:14 -0300

    :recycle: refactor: Aprimorando a classe  perfilOcupacional

--- src/cbo/domain/model/entities/perfil-ocupacional.entity.ts
+++ src/cbo/domain/model/entities/perfil-ocupacional.entity.ts
@@ -1,27 +1,53 @@
+import {Familia} from './familia.entity'
+import {GrandeGrupo} from './grande-grupo.entity'
+import {Ocupacao} from './ocupacao.entity'
+import {SubGrupoPrincipal} from './sub-grupo-principal.entity'
+import {SubGrupo} from './sub-grupo.entity'
+
 export class PerfilOcupacional {
-    id: string
-    cod_grande_grupo: string
-    cod_subgrupo_principal: string
-    cod_subgrupo: string
-    cod_familia: string
-    cod_ocupacao: string
+    id?: string
+    cod_grande_grupo: string // Código do grande grupo
+    cod_subgrupo_principal: string // Código do subgrupo principal
+    cod_subgrupo: string // Código do subgrupo
+    cod_familia: string // Código da família
+    cod_ocupacao: string // Código da ocupação
     sgl_grande_area: string
     nome_grande_area: string
     cod_atividade: string
     nome_atividade: string
-    constructor(
-        id: string,
-        cod_grande_grupo: string,
-        cod_subgrupo_principal: string,
-        cod_subgrupo: string,
-        cod_familia: string,
-        cod_ocupacao: string,
-        sgl_grande_area: string,
-        nome_grande_area: string,
-        cod_atividade: string,
-        nome_atividade: string
-    ) {
-        this.id = id
+
+    grandeGrupoId?: string
+    grandeGrupo?: GrandeGrupo
+    subGrupoPrincipalId?: string
+    subGrupoPrincipal?: SubGrupoPrincipal
+    subGrupoId?: string
+    subGrupo?: SubGrupo
+    familiaId?: string
+    familia?: Familia
+    ocupacaoId?: string
+    ocupacao?: Ocupacao
+
+    constructor({
+        cod_grande_grupo,
+        cod_subgrupo_principal,
+        cod_subgrupo,
+        cod_familia,
+        cod_ocupacao,
+        sgl_grande_area,
+        nome_grande_area,
+        cod_atividade,
+        nome_atividade,
+    }: {
+    cod_grande_grupo: string;
+    cod_subgrupo_principal: string;
+    cod_subgrupo: string;
+    cod_familia: string;
+    cod_ocupacao: string;
+    sgl_grande_area: string;
+    nome_grande_area: string;
+    cod_atividade: string;
+    nome_atividade: string;
+  }) {
         this.cod_grande_grupo = cod_grande_grupo
         this.cod_subgrupo_principal = cod_subgrupo_principal
         this.cod_subgrupo = cod_subgrupo
@@ -31,5 +57,12 @@ export class PerfilOcupacional {
         this.nome_grande_area = nome_grande_area
         this.cod_atividade = cod_atividade
         this.nome_atividade = nome_atividade
+
+        // Atributos relacionais podem ser inicializados com null ou objetos padrões.
+        this.grandeGrupoId = cod_grande_grupo // Pode ser atualizado ao associar o `GrandeGrupo`
+        this.subGrupoPrincipalId = cod_subgrupo_principal // Relacionamento similar
+        this.subGrupoId = cod_subgrupo // Relacionamento similar
+        this.familiaId = cod_familia // Relacionamento similar
+        this.ocupacaoId = cod_ocupacao // Relacionamento similar
     }
 }
