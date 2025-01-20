1a7101f89c2334097305e65bd4049544ce1ee42e
gravetogeek <edusrm11@gmail.com>
2025-01-20 19:04:18 -0300

    recycle: refactor: Desfazendo relacionamento de classes grande-grupo.entity.ts

--- src/cbo/domain/model/entities/grande-grupo.entity.ts
+++ src/cbo/domain/model/entities/grande-grupo.entity.ts
@@ -1,12 +1,7 @@
-import {PerfilOcupacional} from './perfil-ocupacional.entity'
-import {SubGrupoPrincipal} from './sub-grupo-principal.entity'
-
 export class GrandeGrupo {
     id?: string
     codigo: string
     titulo: string
-    subgruposPrincipais?: SubGrupoPrincipal[] // Opcional
-    perfilOcupacional?: PerfilOcupacional[] // Opcional
     constructor(codigo: string, titulo: string) {
         this.codigo = codigo
         this.titulo = titulo
