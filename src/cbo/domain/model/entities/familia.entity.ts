8bbebbcb9f45f55c66351d973b0c97754bc66d03
gravetogeek <edusrm11@gmail.com>
2025-01-20 19:04:04 -0300

    recycle: refactor: Desfazendo relacionamento de classes familia.entity.ts

--- src/cbo/domain/model/entities/familia.entity.ts
+++ src/cbo/domain/model/entities/familia.entity.ts
@@ -1,15 +1,7 @@
-import {Ocupacao} from './ocupacao.entity'
-import {PerfilOcupacional} from './perfil-ocupacional.entity'
-import {SubGrupo} from './sub-grupo.entity'
-
 export class Familia {
     id?: string
     codigo: string
     titulo: string
-    subGrupoId?: string
-    subGrupo?: SubGrupo
-    ocupacoes?: Ocupacao[] // Opcional, pois pode não haver ocupações associadas inicialmente
-    perfilOcupacional?: PerfilOcupacional[] // Opcional
     constructor(codigo: string, titulo: string) {
         this.codigo = codigo
         this.titulo = titulo
