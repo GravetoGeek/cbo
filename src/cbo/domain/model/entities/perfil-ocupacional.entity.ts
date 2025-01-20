8808ce2d3d6bd5607c9b31d6c5471ba3c484869b
gravetogeek <edusrm11@gmail.com>
2025-01-20 19:56:08 -0300

    recycle: refactor: Desfazendo relacionamento de classes perfil-ocupacional.entity.ts

--- src/cbo/domain/model/entities/perfil-ocupacional.entity.ts
+++ src/cbo/domain/model/entities/perfil-ocupacional.entity.ts
@@ -1,9 +1,3 @@
-import {Familia} from './familia.entity'
-import {GrandeGrupo} from './grande-grupo.entity'
-import {Ocupacao} from './ocupacao.entity'
-import {SubGrupoPrincipal} from './sub-grupo-principal.entity'
-import {SubGrupo} from './sub-grupo.entity'
-
 export class PerfilOcupacional {
     id?: string
     cod_grande_grupo: string // Código do grande grupo
@@ -16,17 +10,6 @@ export class PerfilOcupacional {
     cod_atividade: string
     nome_atividade: string
 
-    grandeGrupoId?: string
-    grandeGrupo?: GrandeGrupo
-    subGrupoPrincipalId?: string
-    subGrupoPrincipal?: SubGrupoPrincipal
-    subGrupoId?: string
-    subGrupo?: SubGrupo
-    familiaId?: string
-    familia?: Familia
-    ocupacaoId?: string
-    ocupacao?: Ocupacao
-
     constructor({
         cod_grande_grupo,
         cod_subgrupo_principal,
@@ -38,31 +21,25 @@ export class PerfilOcupacional {
         cod_atividade,
         nome_atividade,
     }: {
-    cod_grande_grupo: string;
-    cod_subgrupo_principal: string;
-    cod_subgrupo: string;
-    cod_familia: string;
-    cod_ocupacao: string;
-    sgl_grande_area: string;
-    nome_grande_area: string;
-    cod_atividade: string;
-    nome_atividade: string;
-  }) {
-        this.cod_grande_grupo = cod_grande_grupo
-        this.cod_subgrupo_principal = cod_subgrupo_principal
-        this.cod_subgrupo = cod_subgrupo
-        this.cod_familia = cod_familia
-        this.cod_ocupacao = cod_ocupacao
-        this.sgl_grande_area = sgl_grande_area
-        this.nome_grande_area = nome_grande_area
-        this.cod_atividade = cod_atividade
-        this.nome_atividade = nome_atividade
+        cod_grande_grupo: string
+        cod_subgrupo_principal: string
+        cod_subgrupo: string
+        cod_familia: string
+        cod_ocupacao: string
+        sgl_grande_area: string
+        nome_grande_area: string
+        cod_atividade: string
+        nome_atividade: string
+    }) {
+        this.cod_grande_grupo=cod_grande_grupo
+        this.cod_subgrupo_principal=cod_subgrupo_principal
+        this.cod_subgrupo=cod_subgrupo
+        this.cod_familia=cod_familia
+        this.cod_ocupacao=cod_ocupacao
+        this.sgl_grande_area=sgl_grande_area
+        this.nome_grande_area=nome_grande_area
+        this.cod_atividade=cod_atividade
+        this.nome_atividade=nome_atividade
 
-        // Atributos relacionais podem ser inicializados com null ou objetos padrões.
-        this.grandeGrupoId = cod_grande_grupo // Pode ser atualizado ao associar o `GrandeGrupo`
-        this.subGrupoPrincipalId = cod_subgrupo_principal // Relacionamento similar
-        this.subGrupoId = cod_subgrupo // Relacionamento similar
-        this.familiaId = cod_familia // Relacionamento similar
-        this.ocupacaoId = cod_ocupacao // Relacionamento similar
     }
 }
