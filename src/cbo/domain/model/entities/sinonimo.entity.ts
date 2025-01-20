1068668460db1b8d9629c5fea3347efbbdf351c5
gravetogeek <edusrm11@gmail.com>
2025-01-20 19:06:18 -0300

    recycle: refactor: Desfazendo relacionamento de classes sinonimo.entity.ts

--- src/cbo/domain/model/entities/sinonimo.entity.ts
+++ src/cbo/domain/model/entities/sinonimo.entity.ts
@@ -1,11 +1,7 @@
-import {Ocupacao} from './ocupacao.entity'
-
 export class Sinonimo {
     id?: string
     codigo: string
     titulo: string
-    ocupacaoId?: string
-    ocupacao?: Ocupacao
     constructor(codigo: string, titulo: string) {
         this.codigo = codigo
         this.titulo = titulo
