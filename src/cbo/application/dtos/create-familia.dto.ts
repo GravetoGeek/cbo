68e1c26ddbcec730d0f7046c5277fee95f8c9318
gravetogeek <edusrm11@gmail.com>
2025-01-20 03:21:09 -0300

    recycle: refactor: Inclusão de mensagens de validação dto

--- src/cbo/application/dtos/create-familia.dto.ts
+++ src/cbo/application/dtos/create-familia.dto.ts
@@ -1,11 +1,11 @@
 import {IsNotEmpty,IsString} from 'class-validator'
 
 export class CreateFamiliaDto {
-    @IsString()
+    @IsString({message: 'O campo "codigo" deve ser uma string.'})
     @IsNotEmpty({message: 'O campo "codigo" é obrigatório.'})
         codigo: string
 
-    @IsString()
+    @IsString({message: 'O campo "titulo" deve ser uma string.'})
     @IsNotEmpty({message: 'O campo "titulo" é obrigatório.'})
         titulo: string
 }
